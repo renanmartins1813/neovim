74f0f77d53540618d641ad1193de82f467da2ca0
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-06 12:04:27 -0500

    adding mistricky/codesnap.nvim plugin

--- init.lua
+++ init.lua
@@ -353,6 +353,34 @@ require('lazy').setup({
     },
   },
 
+  {
+    'mistricky/codesnap.nvim',
+    build = 'make build_generator',
+    keys = {
+      { '<leader>csc', '<cmd>CodeSnap<cr>', mode = 'x', desc = 'Save selected code snapshot into clipboard' },
+      { '<leader>css', '<cmd>CodeSnapSave<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
+      { '<leader>csh', '<cmd>CodeSnapSaveHighlight<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
+      { '<leader>csa', '<cmd>CodeSnapSaveASCII<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
+    },
+    opts = {
+      mac_window_bar = false,
+      -- title = 'CodeSnap.nvim',
+      code_font_family = 'JetBrainsMono Nerd Font Mono',
+      watermark_font_family = 'Pacifico',
+      watermark = '',
+      bg_theme = 'grape',
+      breadcrumbs_separator = '/',
+      has_breadcrumbs = true,
+      has_line_number = true,
+      show_workspace = false,
+      min_width = 0,
+      bg_x_padding = 122,
+      bg_y_padding = 82,
+      -- bg_padding = 0,
+      save_path = '~/Pictures/Codes/',
+    },
+  },
+
   -- custom plugins end
   --
   --
