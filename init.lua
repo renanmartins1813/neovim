e83813a29dbcc9464424556d7df5ecf23a529d4a
renanmartins1813 <renanmartins1813@gmail.com>
2025-06-26 18:41:12 -0500

    enabling HTML and CSS snippets inside PHP filetype

--- init.lua
+++ init.lua
@@ -941,6 +941,13 @@ require('lazy').setup({
             end,
           },
         },
+        config = function()
+          -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+          --require("luasnip.loaders.from_vscode").lazy_load()
+          --require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+          require('luasnip').filetype_extend('php', { 'html' })
+          require('luasnip').filetype_extend('php', { 'css' })
+        end,
       },
       'saadparwaiz1/cmp_luasnip',
 
